88e5d74826563f66c1a46959a2583116177d1269
Armin Kazemi <arminkz@live.com>
2018-01-02 11:54:22 +0330

    Moved Board Helper Functions

---
+++ src/game/BoardHelper.java
@@ -0,0 +1,227 @@
+package game;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class BoardHelper {
+
+    public static ArrayList<Point> getAllPossibleMoves(int[][] board, int player){
+        ArrayList<Point> result = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if(canPlay(board,player,i,j)){
+                    result.add(new Point(i,j));
+                }
+            }
+        }
+        return result;
+    }
+
+    public static ArrayList<Point> getReversePoints(int[][] board,int player,int i,int j){
+
+        ArrayList<Point> allReversePoints = new ArrayList<>();
+
+        int mi , mj , c;
+        int oplayer = ((player == 1) ? 2 : 1);
+
+        //move up
+        ArrayList<Point> mupts = new ArrayList<>();
+        mi = i - 1;
+        mj = j;
+        while(mi>0 && board[mi][mj] == oplayer){
+            mupts.add(new Point(mi,mj));
+            mi--;
+        }
+        if(mi>=0 && board[mi][mj] == player && mupts.size()>0){
+            allReversePoints.addAll(mupts);
+        }
+
+
+        //move down
+        ArrayList<Point> mdpts = new ArrayList<>();
+        mi = i + 1;
+        mj = j;
+        while(mi<7 && board[mi][mj] == oplayer){
+            mdpts.add(new Point(mi,mj));
+            mi++;
+        }
+        if(mi<=7 && board[mi][mj] == player && mdpts.size()>0){
+            allReversePoints.addAll(mdpts);
+        }
+
+        //move left
+        ArrayList<Point> mlpts = new ArrayList<>();
+        mi = i;
+        mj = j - 1;
+        while(mj>0 && board[mi][mj] == oplayer){
+            mlpts.add(new Point(mi,mj));
+            mj--;
+        }
+        if(mj>=0 && board[mi][mj] == player && mlpts.size()>0){
+            allReversePoints.addAll(mlpts);
+        }
+
+        //move right
+        ArrayList<Point> mrpts = new ArrayList<>();
+        mi = i;
+        mj = j + 1;
+        while(mj<7 && board[mi][mj] == oplayer){
+            mrpts.add(new Point(mi,mj));
+            mj++;
+        }
+        if(mj<=7 && board[mi][mj] == player && mrpts.size()>0){
+            allReversePoints.addAll(mrpts);
+        }
+
+        //move up left
+        ArrayList<Point> mulpts = new ArrayList<>();
+        mi = i - 1;
+        mj = j - 1;
+        while(mi>0 && mj>0 && board[mi][mj] == oplayer){
+            mulpts.add(new Point(mi,mj));
+            mi--;
+            mj--;
+        }
+        if(mi>=0 && mj>=0 && board[mi][mj] == player && mulpts.size()>0){
+            allReversePoints.addAll(mulpts);
+        }
+
+        //move up right
+        ArrayList<Point> murpts = new ArrayList<>();
+        mi = i - 1;
+        mj = j + 1;
+        while(mi>0 && mj<7 && board[mi][mj] == oplayer){
+            murpts.add(new Point(mi,mj));
+            mi--;
+            mj++;
+        }
+        if(mi>=0 && mj<=7 && board[mi][mj] == player && murpts.size()>0){
+            allReversePoints.addAll(murpts);
+        }
+
+        //move down left
+        ArrayList<Point> mdlpts = new ArrayList<>();
+        mi = i + 1;
+        mj = j - 1;
+        while(mi<7 && mj>0 && board[mi][mj] == oplayer){
+            mdlpts.add(new Point(mi,mj));
+            mi++;
+            mj--;
+        }
+        if(mi<=7 && mj>=0 && board[mi][mj] == player && mdlpts.size()>0){
+            allReversePoints.addAll(mdlpts);
+        }
+
+        //move down right
+        ArrayList<Point> mdrpts = new ArrayList<>();
+        mi = i + 1;
+        mj = j + 1;
+        while(mi<7 && mj<7 && board[mi][mj] == oplayer){
+            mdrpts.add(new Point(mi,mj));
+            mi++;
+            mj++;
+        }
+        if(mi<=7 && mj<=7 && board[mi][mj] == player && mdrpts.size()>0){
+            allReversePoints.addAll(mdrpts);
+        }
+
+        return allReversePoints;
+    }
+
+    public static boolean canPlay(int[][] board,int player,int i,int j){
+
+        if(board[i][j] != 0) return false;
+
+        int mi , mj , c;
+        int oplayer = ((player == 1) ? 2 : 1);
+
+        //move up
+        mi = i - 1;
+        mj = j;
+        c = 0;
+        while(mi>0 && board[mi][mj] == oplayer){
+            mi--;
+            c++;
+        }
+        if(mi>=0 && board[mi][mj] == player && c>0) return true;
+
+
+        //move down
+        mi = i + 1;
+        mj = j;
+        c = 0;
+        while(mi<7 && board[mi][mj] == oplayer){
+            mi++;
+            c++;
+        }
+        if(mi<=7 && board[mi][mj] == player && c>0) return true;
+
+        //move left
+        mi = i;
+        mj = j - 1;
+        c = 0;
+        while(mj>0 && board[mi][mj] == oplayer){
+            mj--;
+            c++;
+        }
+        if(mj>=0 && board[mi][mj] == player && c>0) return true;
+
+        //move right
+        mi = i;
+        mj = j + 1;
+        c = 0;
+        while(mj<7 && board[mi][mj] == oplayer){
+            mj++;
+            c++;
+        }
+        if(mj<=7 && board[mi][mj] == player && c>0) return true;
+
+        //move up left
+        mi = i - 1;
+        mj = j - 1;
+        c = 0;
+        while(mi>0 && mj>0 && board[mi][mj] == oplayer){
+            mi--;
+            mj--;
+            c++;
+        }
+        if(mi>=0 && mj>=0 && board[mi][mj] == player && c>0) return true;
+
+        //move up right
+        mi = i - 1;
+        mj = j + 1;
+        c = 0;
+        while(mi>0 && mj<7 && board[mi][mj] == oplayer){
+            mi--;
+            mj++;
+            c++;
+        }
+        if(mi>=0 && mj<=7 && board[mi][mj] == player && c>0) return true;
+
+        //move down left
+        mi = i + 1;
+        mj = j - 1;
+        c = 0;
+        while(mi<7 && mj>0 && board[mi][mj] == oplayer){
+            mi++;
+            mj--;
+            c++;
+        }
+        if(mi<=7 && mj>=0 && board[mi][mj] == player && c>0) return true;
+
+        //move down right
+        mi = i + 1;
+        mj = j + 1;
+        c = 0;
+        while(mi<7 && mj<7 && board[mi][mj] == oplayer){
+            mi++;
+            mj++;
+            c++;
+        }
+        if(mi<=7 && mj<=7 && board[mi][mj] == player && c>0) return true;
+
+        //when all hopes fade away
+        return false;
+    }
+
+}
